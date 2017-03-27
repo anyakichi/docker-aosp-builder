0c668a792da50d3b3e33bc804a1fdb9caabd3749
INAJIMA Daisuke <inajima@sopht.jp>
2017-03-27 18:43:48 +0900

    Squashed 'buildenv/' changes from c769e0c..721aee7
    721aee7 entrypoint: Use gosu instead of sudo
    c6d823b entrypoint: Drop -s option from sudo
    
    git-subtree-dir: buildenv
    git-subtree-split: 721aee7a252ad44f697de87903e8b55a4c915651

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,8 +21,8 @@ fi
 
 if [ $# -ne 0 ]; then
     if buildenv "$1" -h >/dev/null 2>&1; then
-        exec sudo -EHsu ${BUILD_USER} buildenv "$@"
+        exec gosu ${BUILD_USER} buildenv "$@"
     else
-        exec sudo -EHsu ${BUILD_USER} "$@"
+        exec gosu ${BUILD_USER} "$@"
     fi
 fi
